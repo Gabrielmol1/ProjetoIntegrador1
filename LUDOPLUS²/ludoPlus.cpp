146b2e7d3a7c1a31c9db95c80f4d3d162f9b1847
Carloscwb1 <carlosgmcol13@gmail.com>
2024-07-01 10:43:43 -0300

    gg
    gg

--- LUDOPLUS²/ludoPlus.cpp
+++ LUDOPLUS²/ludoPlus.cpp
@@ -1134,6 +1134,7 @@ bool moverPeao(vector<vector<char>> &tabuleiro, char cor, int movimentos, const 
                 break;
             }
         }
+
         if (encontrado)
             break;
     }
@@ -1941,3 +1942,12 @@ else
 //erro de passada de vector em ranking vitorias 
 //tela historico esta funcionando
 //
+
+/*const std::vector<std::pair<int, int>> pathPlayer1 = {
+    {6, 2}, {6, 3}, {6, 4}, {6, 5}, {6, 6}, {5, 6}, {4, 6}, {3, 6}, {2, 6}, {1, 6},
+    {1, 7}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8}, {6, 8}, {6, 9}, {6, 10}, {6, 11},
+    {6, 12}, {6, 13}, {7, 13}, {8, 13}, {8, 12}, {8, 11}, {8, 10}, {8, 9}, {8, 8}, {9, 8},
+    {10, 8}, {11, 8}, {12, 8}, {13, 8}, {13, 7}, {13, 6}, {12, 6}, {11, 6}, {10, 6}, {9, 6},
+    {8, 6}, {8, 5}, {8, 4}, {8, 3}, {8, 2}, {8, 1}, {7, 1}, {7, 2}, {7, 3}, {7, 4}, {7, 5},
+    {7, 6}, {7, 7}
+};*/
